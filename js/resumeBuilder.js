fd7e292b5da51f1cb4217afc88068af3a8ec5950
jacnik <jacnik@bitbucket.org>
2015-07-13 09:36:01 +0200

    initial objects created

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,4 +1,79 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
- $("#main").append("Jaca");
+var bio = {
+    "name": "Jacek Doe",
+    "role": "software engineer",
+    "contacts" : {
+        "mobile": "555-555-555",
+        "email": "jacnik@github.com",
+        "twitter": "@jacnik",
+        "github": "jacnik",
+        "location": "Katowice"
+    },
+    "welcomeMsg": "Just welcome",
+    "skills": ["python", "c#", "js"],
+    "picture": "images/fry.jpg"
+};
+
+var education = {
+    "schools": [
+        {
+            "name": "Silesian University of Technology",
+            "city": "Gliwice",
+            "degree": "masters",
+            "majors": ["CS"],
+            "dates": 2010,
+            "url": "http://example.com"
+        }
+    ],
+    "onLineCourses": [
+        {
+            "title": "JavaScript fundamentals",
+            "school": "Udacity",
+            "dates": 2015,
+            "url": "https://www.udacity.com/course/ud804"
+        }
+    ]
+};
+
+var work = {
+    "jobs": [
+        {
+            "employer": "Planet Express",
+            "title": "Delivery boy",
+            "dates": "January 3000 - Future",
+            "description": "some cool long description"
+        }
+    ]
+};
+
+
+var projects = {
+    "projects": [
+        {
+            "title": "Sample project 1"
+            "dates": "2014"
+            "description": "Cool long sample project 1 description"
+            "images": [
+                "http://www.google.pl/imgres?imgurl=http://
+                    www.ecyc.org/sites/default/files/styles/medium/public/page/projects.jpg
+                        %253Fitok%253D0BkKWUO5&imgrefurl=
+                            http://www.ecyc.org/projects&h=278&w=522&tbnid=0UGmUR_GIstLHM:
+                                &zoom=1&docid=xndt4SwT5qNgJM&ei=ZWmjVZGlAoLTygPQrIfQDA&tbm=
+                                    isch&ved=0CCEQMygCMAJqFQoTCNHrx7fL18YCFYKpcgodUNYByg"
+                ,"http://www.google.pl/imgres?imgurl=http://
+                    e36261ef1f37a9aa564c-183246622d51cc66cab6f7d435863a72.r86.cf5.rackcdn.com/
+                        wp-content/uploads/2015/03/Project-Image.jpg%253Fa1f070&imgrefurl=
+                            http://www.customstoday.com.pk/
+                                china-new-energy-projects-in-hungary-and-thailand-proceeding-well-2/
+                                    &h=571&w=1000&tbnid=sLA5YmnK07UyrM:&zoom=1&docid=WDwHxjs5YrAHcM&ei=
+                                        ZWmjVZGlAoLTygPQrIfQDA&tbm=isch&ved=0CCAQMygBMAFqFQoTCNHrx7fL18YCFYKpcgodUNYByg"
+            ]
+        }
+    ]
+};
+
+
+// $("#header").prepend(bio.role);
+// $("#header").prepend(bio.name);
